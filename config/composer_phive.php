3cef09332b3364ce5a69105fdea9939e3fdd27e4
Florian Krämer <florian.kraemer@kreative-design.net>
2020-06-29 16:07:47 +0200

    CI

--- config/composer_phive.php
+++ config/composer_phive.php
@@ -12,7 +12,18 @@ if (!file_exists('.' . $ds . 'phive.phar')) {
     file_put_contents('.' . $ds . 'phive.phar', file_get_contents('https://phar.io/releases/phive.phar'));
 }
 
+$keys = [
+    '0x4AA394086372C20A',
+    '0x8E730BA25823D8B5',
+    '0x31C7E470E2138192',
+    '0x4AA394086372C20A',
+    '0x0F9684B8B16B7AB0',
+    '0xBB5F005D6FFDD89E',
+    '0xCF1A108D0E7AE720'
+];
+$keys = implode(',', $keys);
+
 $output = '';
-exec('php .' . $ds . 'phive.phar install --target ./bin --trust-gpg-keys --trust-gpg-keys 0x4AA394086372C20A,0x8E730BA25823D8B5,0x31C7E470E2138192,0x4AA394086372C20A,0x0F9684B8B16B7AB0,0xBB5F005D6FFDD89E,0xCF1A108D0E7AE720', $output);
+exec('php .' . $ds . 'phive.phar install --target ./bin --trust-gpg-keys --trust-gpg-keys ' . $keys, $output);
 
 echo implode(PHP_EOL, $output);
