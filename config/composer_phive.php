5fbc4e269091be2f6d56adbcf57626e869229be2
Florian Krämer <florian.kraemer@kreative-design.net>
2020-06-29 16:02:40 +0200

    CI

--- config/composer_phive.php
+++ config/composer_phive.php
@@ -13,6 +13,6 @@ if (!file_exists('.' . $ds . 'phive.phar')) {
 }
 
 $output = '';
-exec('php .' . $ds . 'phive.phar install', $output);
+exec('php .' . $ds . 'phive.phar install --target ./bin --trust-gpg-keys --trust-gpg-keys 0x4AA394086372C20A,0x8E730BA25823D8B5,0x31C7E470E2138192,0x4AA394086372C20A,0x0F9684B8B16B7AB0,0xBB5F005D6FFDD89E,0xCF1A108D0E7AE720', $output);
 
 echo implode(PHP_EOL, $output);
